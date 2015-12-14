c3a6cdcd316d6f3efe977bfb607131e31fd4f5ea
sorenpraestegaard <soren_praestegaard@hotmail.com>
2015-12-14 21:41:22 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
-{
-  "name": "d11bot",
-  "version": "1.0.0",
-  "description": "Slackbot version of the legendary d11bot",
-  "main": "App.js",
-  "scripts": {
-    "test": "echo \"Error: no test specified\" && exit 1"
-  },
-  "author": "soren",
-  "license": "MIT",
-  "dependencies": {
-    "body-parser": "^1.12.2",
-    "express": "^4.12.3",
-    "lodash": "^3.6.0"
-  }
-}
+var express = require('express');
+var bodyParser = require('body-parser');
+var d11bot = require('./d11bot');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+// body parser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// test route
+app.get('/', function(request, response) {
+
+  response
+    .status(200)
+    .send('Hello World!');
+
+});
+
+// hello route
+app.post('/hello', d11bot);
+
+// error handler
+app.use(function(error, request, response, next) {
+
+  console.error(error.stack);
+
+  response
+    .status(400)
+    .send(error.message);
+
+});
+
+app.listen(port, function() {
+
+  console.log('d11bot listening on port ' + port);
+
+});
