0a883dc1002990750a7854aef9c48ffa7100d2b8
sorenpraestegaard <soren_praestegaard@hotmail.com>
2015-12-14 21:32:24 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+{
+  "name": "d11bot",
+  "version": "1.0.0",
+  "description": "Slackbot version of the legendary d11bot",
+  "main": "App.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "soren",
+  "license": "MIT",
+  "dependencies": {
+    "body-parser": "^1.12.2",
+    "express": "^4.12.3",
+    "lodash": "^3.6.0"
+  }
+}
