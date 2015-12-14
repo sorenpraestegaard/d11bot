c4b3e8405cdfb64a69e3e92fd43e46f67610424c
sorenpraestegaard <soren_praestegaard@hotmail.com>
2015-12-14 21:09:37 +0100

    Create App.js

---
+++ App.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var hellobot = require('./d11bot');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+// body parser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// test route
+app.get('/', function(request, response) {
+
+  response
+    .status(200)
+    .send('Hello World!');
+
+});
+
+// hello route
+app.post('/hello', d11bot);
+
+// error handler
+app.use(function(error, request, response, next) {
+
+  console.error(error.stack);
+
+  response
+    .status(400)
+    .send(error.message);
+
+});
+
+app.listen(port, function() {
+
+  console.log('d11bot listening on port ' + port);
+
+});
